c74174c0ff481621e09b33fd0a6a2ef428fbc666
Deepika-Titin <deepikaravi081@gmail.com>
2021-01-20 04:07:24 +0530

    done

--- src/org/demo/GitPushD1.java
+++ src/org/demo/GitPushD1.java
@@ -3,6 +3,8 @@ package org.demo;
 public class GitPushD1 {
 
 	public static void main(String[] args) {
+		int a1=20;
+		System.out.println("im making conflicts");
 		System.out.println("Hello Git...");
 		System.out.println("im going to become branch");
 		int a=10;
