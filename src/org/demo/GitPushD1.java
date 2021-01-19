4fdbc675a3686ad79541c4aa50cb27d5e5683205
Deepika-Titin <deepikaravi081@gmail.com>
2021-01-20 03:47:47 +0530

    B branch done

--- src/org/demo/GitPushD1.java
+++ src/org/demo/GitPushD1.java
@@ -5,7 +5,7 @@ public class GitPushD1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git...");
 		System.out.println("im going to become branch");
-		System.out.println("now im in branch B");
+		int a=10;
 	}
 
 }
